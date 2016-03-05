d9a2baeaaf09e776b8a4a837c180be922cf8d9ec
everett980 <everett980@gmail.com>
2016-03-05 15:44:53 -0500

    made buttons work

--- public/MyComponent/subcomponents/SubComponentA.jsx
+++ public/MyComponent/subcomponents/SubComponentA.jsx
@@ -11,29 +11,29 @@ export default class SubComponentA extends React.Component {
   	render() {
   		// let todayDate = (new Date(this.props.evt.date));
   		var styleSpan = {
-  			color : 'white'
+  			color : 'white',
+			width: '66%',
+			float: 'left'
   		};
   		var title = this.props.evt.title;
   		var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' }
     	var date = new Date(+this.props.evt.date).toLocaleDateString('en-US', options);
 
 	    return (
-	    	<div>
-	    	<div class="row" id={title.slice(0,2)}>
-	    		<p style={styleSpan} class="col-xs-8">
+	    	<div class="row" id={title.slice(0,2)} style={{width: '100%'}}>
+	    		<div style={styleSpan}>
 			    	<span> 
 			    		{title}
 			    	</span>
 			    	<span > 
 			    		{date}
 			    	</span>
-			    </p>
-			    <div className="col-xs-4">
+			    </div>
+			    <div style={{width: '33%', float: 'right'}}>
 			    	<button id={title.slice(0,1)} className="btn btn-submit btn-xs" onClick={this.subscribe.bind(this)}>
 			    		Subscribe
 			    	</button>
 		    	</div>
-	    	</div>
 	    	</div>
 	    );
   	}

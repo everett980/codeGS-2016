e69cb188a9426b418d33035248eb81bbb0565071
khumphrey <khumphrey7890@gmail.com>
2016-03-05 11:19:19 -0500

    added some subcomponents

--- public/MyComponent/subcomponents/SubComponentA.jsx
+++ public/MyComponent/subcomponents/SubComponentA.jsx
@@ -6,9 +6,14 @@ export default class SubComponentA extends React.Component {
 	 * Create component HTML
 	 * @return {[type]} [description]
 	 */
+	subscribe () {
+		alert('We are subscribing!');
+	}
+	
   	render() {
 	    return (
-	    	<p style={{color : 'red'}}> Subcomponent says: {this.props.hello}</p>
+	    	<p style={{color : 'red'}}> Subcomponent: {this.props.title}</p> 
+	    	<button class="btn btn-submit" onClick="{this.subscribe.bind(this)}">Subscribe</button>
 	    );
   	}
 }

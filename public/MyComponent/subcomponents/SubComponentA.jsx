72791c982eca1848942e4d1c9375ec7534d94306
khumphrey <khumphrey7890@gmail.com>
2016-03-05 13:01:25 -0500

    yes

--- public/MyComponent/subcomponents/SubComponentA.jsx
+++ public/MyComponent/subcomponents/SubComponentA.jsx
@@ -10,14 +10,14 @@ export default class SubComponentA extends React.Component {
   	render() {
 	    return (
 	    	<div>
-	    	<span style={{color : 'white'}}> {this.props.title} on {this.props.date} </span> 
+	    	<span style={{color : 'white'}}> {this.props.evt.title} on {this.props.evt.date} </span> 
 	    	<button className="btn btn-submit btn-xs" onClick={this.subscribe.bind(this)}>Subscribe</button>
 	    	</div>
 	    );
   	}
 	subscribe () {
 		alert('We are subscribing here!');
-		
+
 		//change button to say subscribed
 		//have an event created that waits until the appropriate dateTime
 		//when the time comes have the locationUrl popup
